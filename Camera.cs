8e7c58452dda303c98e47da30d36a4d29a3209f2
yasyasyu <55916047+yasyasyu@users.noreply.github.com>
2021-01-29 22:25:00 +0900

    Add files via upload

---
+++ Camera.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public float sensitivityX = 15F;//マウスの横の動きの強さ
+    public float sensitivityY = 15F;//マウスの縦の動きの強さ
+
+    public float minimumX = -360F;//横の回転の最低値
+    public float maximumX = 360F;//横の回転の最大値
+
+    public float minimumY = -60F;//縦の回転の最低値
+    public float maximumY = 60F;//縦の回転の最大値
+
+    public float rotationX = 0f;        /*今回の要*/
+    float rotationY = 0f;
+
+    public GameObject Player;   //PlayerObject
+
+    //関数
+    public float retrotationX(){
+        return rotationX;
+    }
+    //プロパティ
+    public float getrotationX{
+        get{ return this.rotationX; }
+    }
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        transform.position = Player.transform.position;//座標をプレイヤー依存にする
+
+
+        rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;//rotationXを現在のyの向きにXの移動量*sensitivityXの分だけ回転させる
+        rotationY += Input.GetAxis("Mouse Y") * sensitivityY;//rotationYにYの移動量*sensitivityYの分だけ増やす
+        rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);//rotationYを-60〜60の値にする
+
+        transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);//オブジェクトの向きをnew Vector3(-rotationY, rotationX, 0)にする
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	[SerializeField]// SerializeFieldと書くとその次の変数がエディタ上で数値を調整可能になる
+	private float JumpPower = 400;// ジャンプ力
+
+	[SerializeField]
+	private float MoveSpeed = 10;// 移動スピード
+
+	private bool Grounded;// 地面についているか判定する変数
+
+	private Rigidbody rb;// Rigidbodyを扱うための変数
+
+	public float sensitivityX = 15F;//マウスの横の動きの強さ
+	public float sensitivityY = 15F;//マウスの縦の動きの強さ
+
+	public float minimumX = -360F;//横の回転の最低値
+	public float maximumX = 360F;//横の回転の最大値
+
+	public float minimumY = -60F;//縦の回転の最低値
+	public float maximumY = 60F;//縦の回転の最大値
+
+	public GameObject mainCamera;
+	Camera cameraClass;
+
+	float rotationX;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();// Rigidbodyの値を取得してrbに代入する
+		cameraClass = mainCamera.GetComponent<Camera>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.W))// もし、Wキーがおされたら、
+		{
+			 transform.position += transform.TransformDirection(Vector3.forward * MoveSpeed * Time.deltaTime);// 前方に進む
+		}
+
+		if (Input.GetKey(KeyCode.S))// もし、Sキーがおされたら、
+		{
+			transform.position += transform.TransformDirection(Vector3.back * MoveSpeed * Time.deltaTime);// 後方に進む
+		}
+	    if (Input.GetKey(KeyCode.A))// もし、Aキーがおされたら、
+        {
+            transform.position += transform.TransformDirection(Vector3.left * MoveSpeed * Time.deltaTime);// 左に進む
+        }
+        if (Input.GetKey(KeyCode.D))// もし、Dキーがおされたら、
+        {
+            transform.position += transform.TransformDirection(Vector3.right * MoveSpeed * Time.deltaTime);// 右に進む
+        }
+
+		if (Grounded == true)//  もし、Groundedがtrueなら、
+		{
+			if (Input.GetKeyDown(KeyCode.Space))//  もし、スペースキーがおされたなら、  
+			{
+				Grounded = false;//  Groundedをfalseにする
+				rb.AddForce(Vector3.up * JumpPower);//  上にJumpPower分力をかける
+			}
+		}
+		if(transform.position.y < -30){//落下したら
+			transform.position = new Vector3(0,20,0);
+			rb.velocity = Vector3.zero;
+		}
+
+		/*ここから本題*/
+		// //public変数
+		rotationX = cameraClass.rotationX;
+		//プロパティ
+		// rotationX = cameraClass.getrotationX;
+		// //関数
+		// rotationX = cameraClass.retrotationX();
+
+		Debug.Log(rotationX);//変数が受け取れているか確認
+		transform.localEulerAngles = new Vector3(0,rotationX,0);//Playerの向きを設定する。
+	}
+
+	void OnCollisionEnter(Collision other)//  地面に触れた時の処理
+	{
+		if (other.gameObject.tag == "Ground")//  もしGroundというタグがついたオブジェクトに触れたら、
+		{
+			Grounded = true;//  Groundedをtrueにする
+		}
+	}
+}
